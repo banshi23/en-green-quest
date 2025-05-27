8cf9366f1f05fb6d0510793da018888b97fa144d
Banshilal Mahata <banshilal@sentientgeeks.com>
2025-05-27 21:20:02 +0530

    Id redirect

--- src/app/pages/cookie-policy/cookie-policy.component.ts
+++ src/app/pages/cookie-policy/cookie-policy.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cookie-policy',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './cookie-policy.component.html',
   styleUrl: './cookie-policy.component.scss'
 })
